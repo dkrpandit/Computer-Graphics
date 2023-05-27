6aab8b3a4e3a59eabdd277f781cbf97e36374085
Dharmendra Pandit <99905700+dkrpandit@users.noreply.github.com>
2023-05-28 00:09:11 +0530

    Update DDA.cpp

--- DDA and Bresenham line drawing algorithm/DDA.cpp
+++ DDA and Bresenham line drawing algorithm/DDA.cpp
@@ -1,1 +1,9 @@
-
+/*
+2. Implement DDA and Bresenham line drawing algorithm to draw: 
+i) Simple Line 
+ii) Dotted Line
+iii) Dashed Line
+iv) Solid line 
+using mouse interface Divide the screen in four quadrants with
+center as (0, 0). The line should work for all the slopes positive as well as negative.
+*/
