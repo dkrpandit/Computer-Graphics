a49f9139afcacdac7cc15e9d42051c3288374c5a
Dharmendra Pandit <99905700+dkrpandit@users.noreply.github.com>
2023-06-05 17:26:39 +0530

    Update DDA.cpp

--- DDA and Bresenham line drawing algorithm/DDA.cpp
+++ DDA and Bresenham line drawing algorithm/DDA.cpp
@@ -10,72 +10,74 @@ center as (0, 0). The line should work for all the slopes positive as well as ne
 #include <iostream>
 #include <GL/glut.h>
 using namespace std;
-int xa, ya, xb, yb;
-int lineType;
 
-int Round(float a)
+int xa, ya, xb, yb;  // Variables to store coordinates of the line
+int lineType;        // Variable to store the type of line
+int Round(float num) // Function to round off a floating-point number to the nearest integer
 {
-    return (int)(a + 0.5);
+    return (int)(num + 0.5);
 }
 
-void ddaline(int xa, int ya, int xb, int yb, int lineType)
+void dda(int xa, int ya, int xb, int yb, int lineType)
 {
-    int dx, dy, steps;
+    int dx, dy;
     dx = xb - xa;
     dy = yb - ya;
 
+    int step;
     if (abs(dx) > abs(dy))
     {
-        steps = abs(dx);
+        step = abs(dx);
     }
     else
     {
-        steps = abs(dy);
+        step = abs(dy);
     }
-    float xincrement, yincrement;
-    xincrement = dx / (float)(steps);
-    yincrement = dy / (float)(steps);
+
+    float xIncrement, yIncrement;
+
+    xIncrement = dx / float(step);
+    yIncrement = dy / float(step);
 
     float x, y;
     x = xa;
     y = ya;
 
     glBegin(GL_POINTS);
-    glVertex2d(Round(x), Round(y));
 
     int k;
-    for (k = 0; k < steps; k++)
+
+    for (k = 0; k < step; k++)
     {
-        if (lineType == 4)
+        switch (lineType)
         {
-            glPointSize(3.0);
-            x = x + xincrement;
-            y = y + yincrement;
+        case 1: // Simple Line
+            x = x + xIncrement;
+            y = y + yIncrement;
             glVertex2d(Round(x), Round(y));
-        }
-        if (lineType == 3)
-        {
-            if (k % 4 != 0)
-            {
-                glVertex2d(Round(x), Round(y));
-            }
-            x = x + xincrement;
-            y = y + yincrement;
-        }
-        else if (lineType == 2)
-        {
+            break;
+        case 2: // Dotted Line
             if (k % 4 == 0)
             {
                 glVertex2d(Round(x), Round(y));
             }
-            x = x + xincrement;
-            y = y + yincrement;
-        }
-        else
-        {
-            x = x + xincrement;
-            y = y + yincrement;
+            x = x + xIncrement;
+            y = y + yIncrement;
+            break;
+        case 3: // Dashed Line
+            if (k % 4 != 0)
+            {
+                glVertex2d(Round(x), Round(y));
+            }
+            x = x + xIncrement;
+            y = y + yIncrement;
+            break;
+        case 4: // Solid Line
+            glPointSize(3.0);
+            x = x + xIncrement;
+            y = y + yIncrement;
             glVertex2d(Round(x), Round(y));
+            break;
         }
     }
 
@@ -85,48 +87,50 @@ void ddaline(int xa, int ya, int xb, int yb, int lineType)
 
 void mouse1(int button, int state, int x, int y)
 {
-    y = 480 - y;
+    y = 440 - y; // Adjust the y-coordinate to match the window's coordinate system
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        xa = x - 320; // Shifting x-coordinate to center (0,0)
-        ya = y - 240; // Shifting y-coordinate to center (0,0)
+        xa = x - 320; // Calculate the initial x-coordinate relative to the center of the window
+        ya = y - 240; // Calculate the initial y-coordinate relative to the center of the window
     }
     if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
     {
-        xb = x - 320; // Shifting x-coordinate to center (0,0)
-        yb = y - 240; // Shifting y-coordinate to center (0,0)
-        glColor3f(1.0, 0.0, 0.0);
-        ddaline(xa, ya, xb, yb, lineType);
+        xb = x - 320;                  // Calculate the final x-coordinate relative to the center of the window
+        yb = y - 240;                  // Calculate the final y-coordinate relative to the center of the window
+        glColor3f(1.0, 0.0, 0.0);      // Set color to red
+        dda(xa, ya, xb, yb, lineType); // Call the DDA function to draw the line
     }
 }
-void Init()
+
+void draw()
 {
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glColor3f(1.0, 0.0, 0.0);
-    gluOrtho2D(-320, 320, -240, 240); // Set the coordinate system
-}
-void Draw()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0, 1.0, 0.0);
-    ddaline(-320, 0, 320, 0, 1); // X-axis
-    ddaline(0, -240, 0, 240, 1); // Y-axis
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
+    glColor3f(0.0, 1.0, 0.0);     // Set color to green
+    dda(-320, 0, 320, 0, 1);      // Draw x-axis using DDA algorithm
+    dda(0, -240, 0, 240, 1);      // Draw y-axis using DDA algorithm
 }
 
 int main(int argc, char **argv)
 {
-    cout << "Enter what type of line to draw \n1. Simple \n2. Dotted \n3. Dashed \n4. Solid :-  ";
-    cin >> lineType;
+    cout << "Enter \n";
+    cout << "1. Simple Line \n";
+    cout << "2. Dotted Line \n";
+    cout << "3. Dashed Line \n";
+    cout << "4. Solid Line \n";
+    cin >> lineType; // Read the line type from user input
 
     glutInit(&argc, argv);
-    glutInitWindowPosition(0, 0);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
-    glutCreateWindow("DDA Line");
-    Init();
-    glutDisplayFunc(Draw);
-    glutMouseFunc(mouse1);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("1st Assignment");
+    gluOrtho2D(-320, 320, -240, 240); // Set the orthographic projection
+    glutDisplayFunc(draw);            // Register the draw function to be called when the window needs to be redrawn
+    glutMouseFunc(mouse1);            // Register the mouse function to handle mouse events
+    glutMainLoop();                   // Start the main loop
 
-    glutMainLoop();
     return 0;
 }
+
+//dharmendra@pandit:~$ g++ test.cpp -lGL -lGLU -lglut
+//dharmendra@pandit:~$ ./a.out
